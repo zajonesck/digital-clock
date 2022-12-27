c6851cce30591487a351eb74566be6bbc389f5d3
Zack Jones <zajonesck83@gmail.com>
2022-12-27 03:05:18 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const dog = "some string";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div className="Dog">"woof"</div>
+        <div>{dog}</div>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
